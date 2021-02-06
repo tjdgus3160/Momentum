4e6ed8221bad9777d7c1d6212275bea3fe345246
tjdgus3160 <tjdgus3160@gmail.com>
2021-02-06 14:48:58 +0900

    모듈화

---
+++ src/main.js
@@ -0,0 +1,6 @@
+document.write("<script src='./utils/clock.js'></script>");
+document.write("<script src='./components/gretting.js'></script>");
+document.write("<script src='./components/todo.js'></script>");
+document.write("<script src='./components/bg.js'></script>");
+document.write("<script type='text/javascript' src='./api/apikey.json'></script>");
+document.write("<script src='./utils/weather.js'></script>");
