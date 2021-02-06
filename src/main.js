499ef440770b91770999fd9b08b098fb3b67b007
tjdgus3160 <tjdgus3160@gmail.com>
2021-02-06 14:59:53 +0900

    git page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-document.write("<script src='./utils/clock.js'></script>");
-document.write("<script src='./components/gretting.js'></script>");
-document.write("<script src='./components/todo.js'></script>");
-document.write("<script src='./components/bg.js'></script>");
-document.write("<script type='text/javascript' src='./api/apikey.json'></script>");
-document.write("<script src='./utils/weather.js'></script>");
+document.write("<script src='./src/utils/clock.js'></script>");
+document.write("<script src='./src/components/gretting.js'></script>");
+document.write("<script src='./src/components/todo.js'></script>");
+document.write("<script src='./src/components/bg.js'></script>");
+document.write("<script type='text/javascript' src='./src/api/apikey.json'></script>");
+document.write("<script src='./src/utils/weather.js'></script>");
